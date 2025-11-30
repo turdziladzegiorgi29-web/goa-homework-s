d00a1fe39e0679dfed807e4b4ecb8c8de38a93af
turdziladzegiorgi29-web <turdziladzegiorgi29@gmail.com>
2025-11-30 16:33:26 +0400

    commit69

---
+++ day 02/claswork/classwork2.py
@@ -0,0 +1,15 @@
+food="burger"
+print(food)
+print(69)
+print(6.9)
+food="hot dog"
+print(food)
+print("giorgi "+"turdziladze")
+print("house"*3)
+#this codewill multiply the said word (house) by 3
+print("this is string")
+surname="giorgi"
+car="mercedes"
+lesson="math"
+print(lesson)
+#a variable is a name that stores the value of the code it is writen in with the ability to reuse its data without retyping
---
+++ day 02/homework/homework2.py
@@ -0,0 +1,14 @@
+#concatination is when u combine 2 string using "+" symbol
+brand="ferrari"
+model=458
+acceleration=3.7
+print(68+1)
+print(70-1)
+print(23*3)
+print(4623//67)
+print("giorgi"*20)
+name="students"
+print(name+name)
+number=23
+number1="4"
+print(number+number1)
