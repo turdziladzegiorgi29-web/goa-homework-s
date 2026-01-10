b2fc1b5b20196d4b0e2cbfcddf597efdf445f23a
turdziladzegiorgi29-web <turdziladzegiorgi29@gmail.com>
2026-01-10 19:34:51 +0400

    commit 10

--- day 06/classwok06.py
+++ day 06/classwok06.py
@@ -1,5 +1,5 @@
 #nesting is when code is stacked in a sinle opening and closing piece of code 
 #indentation is the same thing as nesting 
-# the B/string tags it makes text on yur web page bold
-#the i tag makes yiur text tilt 
-#the M tage has tag same effect as the i tag but more "emotional"
+# the B/strong tags it makes text on your web page bold
+#the i tag makes your text tilt 
+#the M tag has the same effect as the i tag but more "emotional"
---
+++ day 09/homework09/homework09.py
@@ -0,0 +1,24 @@
+#string is a code fully made by text an integer is a piece of code made by only numbers a float is a piece of code made only by decimals (for exmaple)
+age = int(input("enter your age"))
+if age >= 25:
+   print("ur under 25 yrs old")
+else:
+   print("you are under 25 yrs old" ) 
+
+name = "giorgi"
+print(type(name))
+
+AGE = 69
+gio = str(AGE)
+print(gio)
+print(type(gio))
+
+a = 50
+b = 100
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
