00cc2b2b392f203e07374723f9a1e5006aefe9b7
turdziladzegiorgi29-web <turdziladzegiorgi29@gmail.com>
2026-01-12 20:18:03 +0400

    commit 11

---
+++ day 11/classwork11/classwork-11.py
@@ -0,0 +1,21 @@
+# control flow is a order in which a programs instruction is executed
+# an algorithm is a instruction that you go through step by step to solve a problem
+# two slashes lets you divide without getting a float (basicly getting an integer instead)
+# a percent is a "operation" that lets us see the "NASHTI" of a dividation of 2 numbers
+
+# num=int(input())
+# name=input()
+# print(num > 15 and name == "gio")
+
+# age=int(input())
+# nam=input()
+# print(age > 18 and nam == "andrew")
+ 
+# nuum= 17
+# print(nuum > 18 or 2 > 1 )
+
+# asaki=18
+# print(asaki >= 18 and 10 == 10)
+
+random = "saba"
+print("saba" == random  or 17 % 2 == 0)
---
+++ day 11/homework11/homework11.py
@@ -0,0 +1,20 @@
+op1=True or True 
+op2=True or False
+op3=False or True
+op4=False or False
+op5= True and True 
+op6=False and False
+op7=True and False
+op8=False and True 
+
+hight=int(input())
+name=input()
+age=int(input())
+print( age > 18 and name == "gio" and hight > 180)
+
+num1=int(input())
+num2=int(input())
+print(num1 % num2)
+
+age1=int(input())
+print(age1 % 2==0)
