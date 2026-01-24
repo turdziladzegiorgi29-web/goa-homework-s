0df1ee4ea01f9ec60880844da298b0a79541dcfa
turdziladzegiorgi29-web <turdziladzegiorgi29@gmail.com>
2026-01-24 19:05:01 +0400

    commit 13

--- day 11/classwork11/classwork-11.py
+++ day 11/classwork11/classwork-11.py
@@ -3,19 +3,19 @@
 # two slashes lets you divide without getting a float (basicly getting an integer instead)
 # a percent is a "operation" that lets us see the "NASHTI" of a dividation of 2 numbers
 
-# num=int(input())
-# name=input()
-# print(num > 15 and name == "gio")
+num=int(input())
+name=input()
+print(num > 15 and name == "gio")
 
-# age=int(input())
-# nam=input()
-# print(age > 18 and nam == "andrew")
+age=int(input())
+nam=input()
+print(age > 18 and nam == "andrew")
  
-# nuum= 17
-# print(nuum > 18 or 2 > 1 )
+nuum= 17
+print(nuum > 18 or 2 > 1 )
 
-# asaki=18
-# print(asaki >= 18 and 10 == 10)
+asaki=18
+print(asaki >= 18 and 10 == 10)
 
 random = "saba"
-print("saba" == random  or 17 % 2 == 0)
+--print("saba" == random  or 17 % 2 == 0)
