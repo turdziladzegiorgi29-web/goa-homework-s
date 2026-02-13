0867a232867c75af7a2a2c938608b2a58ae24ba0
turdziladzegiorgi29-web <turdziladzegiorgi29@gmail.com>
2026-02-13 21:38:49 +0400

    commit 18

---
+++ day 19/classwork19/classwork19.py
@@ -0,0 +1,3 @@
+# slicing is a "operation" that pulls out a specific portion of sting or list and in slicing you cand to from right to left unlike indexing
+group=("giorgi", "alexandre", "lasha", "oto") 
+print(group[1:4])
---
+++ day 19/homework19/homework19.py
@@ -0,0 +1,9 @@
+# name= input()
+# print(name[0])
+# print (name[-1])
+
+# list2=['gio' , 'lasha' , 'alex' , 'oto']
+# print(list2[::-1])
+
+surname=input()
+print(surname[0:5][::-1])
